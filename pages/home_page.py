662a23bdffcd92de461207845436bfed30a3630e
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-19 00:26:17 +0530

    added home page

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,6 +1,12 @@
-
+from selenium.webdriver.common.by import By
 
 
 class HomePage:
     def __init__(self, driver):
         self.driver = driver
+
+    # Locators
+    search_box_xpath = "//input[@id='searchQuery']"
+
+    def enter_product_into_search_box(self, product_name):
+        self.driver.find_element(By.XPATH, self.search_box_xpath).send_keys(product_name)
