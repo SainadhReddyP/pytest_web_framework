18e0b8130b85a088e536a7bb0cda7e9a6a494da5
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-25 12:53:43 +0530

    added get data from excel method to excel_utils.py

--- utilities/excel_utils.py
+++ utilities/excel_utils.py
@@ -24,3 +24,19 @@ def set_cell_data(path, sheet_name, row_number, column_number, data):
     sheet = workbook[sheet_name]
     sheet.cell(row=row_number, column=column_number).value=data
     workbook.save(path)
+
+
+def get_data_from_excel(path, sheet_name):
+    final_list = []
+    workbook = openpyxl.load_workbook(path)
+    sheet = workbook[sheet_name]
+    total_rows = sheet.max_row
+    total_columns = sheet.max_column
+
+    for r in range(2,total_rows+1):
+        row_list = []
+        for c in range(1, total_columns+1):
+            row_list.append(sheet.cell(row=r, column=c).value)
+        final_list.append(row_list)
+
+    return final_list
