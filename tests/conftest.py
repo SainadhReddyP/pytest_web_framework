00531ba38d5b94984af5c5fe10a7d8de583a56b1
SainadhReddyP <meetsainadhreddy@gmail.com>
2023-12-25 17:30:29 +0530

    added test for taking screenshot on failure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,31 @@
 from selenium import webdriver
 from utilities import ReadConfigurations
+from allure_commons.types import AttachmentType
 import pytest
+import allure
+
+
+@pytest.fixture()
+def log_on_failure(request):
+    yield
+    item = request.node
+    if item.rep_call.failed:
+        allure.attach(driver.get_screenshot_as_png(), name="Failed Test",
+                      attachment_type=AttachmentType.PNG)
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
 
 
 @pytest.fixture()
 def setup_and_teardown(request):
     browser = ReadConfigurations.read_configuration("basic_info", "browser")
-    driver = None
+    global driver
     if browser.__eq__("chrome"):
         driver = webdriver.Chrome()
     elif browser.__eq__("firefox"):
@@ -22,3 +41,4 @@ def setup_and_teardown(request):
     request.cls.driver = driver # class level using the fixture
     yield
     driver.quit()
+
